9659a517084fbaa7bd2aeddfcc872a2ef2614e5f
AlexNedJ <nedovj@gmail.com>
2025-07-30 13:42:29 +0300

    add new foder and img

--- alian_invasion.py
+++ alian_invasion.py
@@ -1,21 +1,23 @@
 import sys
-
+from seting import Settings
+from ship import Ship
 import pygame
 
 class AlianInvasion:
     def __init__(self):
         pygame.init()
-        self.bg_color = (230, 230, 230)
-
-        self.screen = pygame.display.set_mode((800, 600))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alian  Invasion")
+        self.ship = Ship(self.screen)
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitime()
             pygame.display.flip()
 if __name__ == '__main__':
     ai = AlianInvasion()
